57749a6b72312452f41ba91d2bf1fe59abdbaee8
harsh14thakre <harsh14thakre@gmail.com>
2025-10-23 13:31:21 +0530

    dic questions

--- mydatastructure.py
+++ mydatastructure.py
@@ -158,3 +158,52 @@
 # Duplicates- Keys must be unique but you can have duplicates in values.
 # Order- Dictionary follows insertion order.
 # Heterogeneous- A dictionary can store different types of keys and values like integers strings lists or even another dictionary.
+
+# QUESTIONS:-
+
+#-> Write a Python script to merge two python dictionaries.
+
+# d1 = {10:100,20:200,40:300}
+# d2 = {40:400,50:500,60:600}
+
+# for i in d2:
+#     d1[i] = d2[i]
+
+# print(d1)    
+
+#-> Print th e sum of dictionary.
+
+# d1 = {10:100,20:200,40:300}
+# sum=0
+
+# for i in d1:
+#     sum = sum + d1[i]
+
+# print(sum)    
+
+#-> Count the frequency of each element in a list.
+
+# a = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,7,8]
+
+# d = {}
+# for i in a:
+#     if i in d.keys():
+#         d[i]+=1
+#     else:
+#         d[i] = 1
+
+# print(d)            
+
+#-> Write a python program to combine the two dictionaries by adding values for common keys.
+
+# d1 = {10:100,20:200,40:300}
+# d2 = {40:400,50:500,60:600}
+
+# for i in d2:
+#     if i in d1.keys():
+#         d1[i] += d2[i]
+#     else:
+#         d1[i] = d2[i] 
+
+# print(d1)           
+ 

c4b41ef82db3493ccf71bd39c23a70bbaae3ab34
harsh14thakre <harsh14thakre@gmail.com>
2025-10-23 14:04:38 +0530

    exception handeling

---
+++ exceptionhandeling.py
@@ -0,0 +1,74 @@
+#--------------------------------Error division by zero----------------------------------------------
+
+# a = int(input("Tell your number:-"))
+
+# try:
+#     print(10/a)
+
+# except Exception as err:
+#     print(f"Sorry there is an error as {err}")
+
+# print("Ok i have dont the division")        
+
+
+
+# a = input("Tell your number:-")
+
+# try:
+#     print(10/a)
+
+# except Exception as err:
+#     print(f"Sorry there is an error as {err}")
+
+# print("Ok i have dont the division")        
+
+# -----------------------------------------else------------------------------------------------------
+
+
+# a = int(input("Tell your number:-"))
+
+# try:
+#     print(10/a)
+
+# except Exception as err:
+#     print(f"Sorry there is an error as {err}")
+
+# else:
+#     print("Good there is no exception")
+
+# print("Ok i have dont the division")        
+
+# ----------------------------------------finally---------------------------------------------------
+
+# Finally always run
+
+
+# a = int(input("Tell your number:-"))
+
+# try:
+#     print(10/a)
+
+# except Exception as err:
+#     print(f"Sorry there is an error as {err}")
+
+# else:
+#     print("Good there is no exception")
+
+# finally:
+#     print("I will run no matter what")    
+
+# print("Ok i have dont the division")        
+
+# ---------------------------------------------raise-------------------------------------------------
+
+# generate error by ourself
+
+# age = int(input("Tell your age:-"))
+
+# if age < 10 or age > 18:
+#     raise ValueError("Your age must be between 10 and 18")
+# else:
+#     print("Welcome to the club")
+
+# print("The club will start soon")    
+
